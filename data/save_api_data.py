f5aa74498388493961b439e889c1a9146a2bd106
Nishant173 <nishant.rao173@gmail.com>
2020-12-14 12:37:17 +0530

    Modified logic that gets API data to store locally in JSON files

--- data/save_api_data.py
+++ data/save_api_data.py
@@ -10,23 +10,20 @@ def save_object_as_json(obj, filepath):
 
 def save_api_data():
     """Saves data fetched from the API to local JSON file/s"""
-    standings = [
-        {"league": "Bundesliga", "season": "2012-13"},
-        {"league": "EPL", "season": "2012-13"},
-        {"league": "La Liga", "season": "2012-13"},
-        {"league": "Ligue 1", "season": "2012-13"},
-        {"league": "Serie A", "season": "2012-13"},
+    leagues = ["Bundesliga", "EPL", "La Liga", "Ligue 1", "Serie A"]
+    seasons = [
+        "2009-10", "2010-11", "2011-12", "2012-13", "2013-14",
+        "2014-15", "2015-16", "2016-17", "2017-18", "2018-19",
     ]
-    for standing in standings:
-        league = standing['league']
-        season = standing['season']
-        url = f"http://localhost:8000/api/v1/league-standings?league={league}&season={season}"
-        response = requests.get(url=url)
-        if response.ok:
-            obj = json.loads(response.text)
-            save_object_as_json(obj=obj, filepath=f"json/{league} ({season}).json")
-        else:
-            print(f"API error for {league} ({season}) data")
+    for league in leagues:
+        for season in seasons:
+            url = f"http://localhost:8000/api/v1/league-standings?league={league}&season={season}"
+            response = requests.get(url=url)
+            if response.ok:
+                obj = json.loads(response.text)
+                save_object_as_json(obj=obj, filepath=f"json/{league} ({season}).json")
+            else:
+                print(f"API error for {league} ({season}) data")
     print("Done")
     return None
 
