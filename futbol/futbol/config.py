9fc45995d04aa52eac2d6bb81f4a4e524596d75e
Nishant173 <nishant.rao173@gmail.com>
2020-12-17 15:30:17 +0530

    Added new table name to config

--- futbol/futbol/config.py
+++ futbol/futbol/config.py
@@ -6,3 +6,4 @@ be used throughout the project.
 DB_FILEPATH = "db.sqlite3"
 TBL_LEAGUE_MATCHES = "leagues_leaguematch"
 TBL_LEAGUE_STANDINGS = "leagues_leaguestandings"
+TBL_CROSS_LEAGUE_STANDINGS = "leagues_crossleaguestandings"
