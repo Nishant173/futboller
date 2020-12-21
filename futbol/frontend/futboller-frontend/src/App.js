f55be6f97a383d9bd02f7a51e6fc1952fc70a802
Nishant173 <nishant.rao173@gmail.com>
2020-12-21 20:05:51 +0530

    Cleaned up frontend code

--- futbol/frontend/futboller-frontend/src/App.js
+++ futbol/frontend/futboller-frontend/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
 import LeagueTableView from './components/LeagueTableView';
 import CrossLeagueTableView from './components/CrossLeagueTableView';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      {/* <LeagueTableView /> */}
-      <CrossLeagueTableView />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/league-standings" component={LeagueTableView} />
+          <Route exact path="/cross-league-standings" component={CrossLeagueTableView} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- futbol/frontend/futboller-frontend/src/components/Navbar.js
+++ futbol/frontend/futboller-frontend/src/components/Navbar.js
@@ -1,38 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from './Button';
 import "./Navbar.css";
 
 
 export default function Navbar() {
     const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
-    const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
-    const showButton = () => {
-        if(window.innerWidth <= 960) {
-            setButton(false);
-        } else {
-            setButton(true);
-        }
-    };
-
-    useEffect(() => {
-        showButton();
-    }, []);
-
-    window.addEventListener("resize", showButton);
     
     return (
         <>
             <nav className="navbar">
                 <nav className="navbar-container">
                     <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                        Futboller <i className="fab fa-typo3" />
+                        Futboller
                     </Link>
-                    <div className="menu-icon" onClick={handleClick}>
-                        <i className={click ? "fas fa-times" : "fas fa-bars"} />
-                    </div>
 
                     <ul className={click ? "nav-menu active" : "nav-menu"}>
                         <li className="nav-item">
@@ -42,7 +23,7 @@ export default function Navbar() {
                         </li>
                         <li className="nav-item">
                             <Link to="/cross-league-standings" className="nav-links" onClick={closeMobileMenu}>
-                            CrossLeagueStandings
+                                CrossLeagueStandings
                             </Link>
                         </li>
                     </ul>
