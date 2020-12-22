8d87ed576573871054a32ce8a02339d64a96c79d
Nishant173 <nishant.rao173@gmail.com>
2020-12-22 12:05:50 +0530

    Updated Django secret key

--- futbol/futbol/settings.py
+++ futbol/futbol/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("FDS_DJANGO_SECRET")
+SECRET_KEY = os.environ.get("FUTBOLLER_DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
