f8668357f59e239b44f0c8678fb9ee060dc30b12
Nishant173 <nishant.rao173@gmail.com>
2021-01-16 18:13:04 +0530

    Fixed status codes

--- futbol/api/views.py
+++ futbol/api/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -6,4 +7,4 @@ from . import docs
 
 @api_view(['GET'])
 def get_documentation(request):
-    return Response(data=docs.ENDPOINTS, status=200)
+    return Response(data=docs.ENDPOINTS, status=status.HTTP_200_OK)
