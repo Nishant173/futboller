c54aa345c1f4f39129128967ed42e20f9e3f180b
Nishant173 <nishant.rao173@gmail.com>
2021-01-21 14:39:21 +0530

    Updated hard-coded season names in frontend container for LeagueStandings

--- futbol/frontend/futboller-frontend/src/components/LeagueStandingsView.js
+++ futbol/frontend/futboller-frontend/src/components/LeagueStandingsView.js
@@ -49,6 +49,8 @@ export default function LeagueStandingsView() {
                     <option value="2016-17">2016-17</option>
                     <option value="2017-18">2017-18</option>
                     <option value="2018-19">2018-19</option>
+                    <option value="2019-20">2019-20</option>
+                    <option value="2020-21">2020-21</option>
                 </select>
                 <input
                     type="button"
