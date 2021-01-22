a3f63fde74b57d32fc4482679417280794e874af
Nishant173 <nishant.rao173@gmail.com>
2021-01-22 23:10:04 +0530

    Changed filename and updated 1 function in same file

--- data_ingestion/metadata_generator.py
+++ data_ingestion/metadata_generator.py
@@ -16,3 +16,9 @@ def get_num_matches_by_league_and_season(data: pd.DataFrame) -> pd.DataFrame:
     df_by_league_and_season = data.groupby(by=['League', 'Season']).apply(len).reset_index()
     df_by_league_and_season.rename(mapper={0: 'NumMatches'}, axis=1, inplace=True)
     return df_by_league_and_season
+
+
+def get_num_unique_teams_by_league(data: pd.DataFrame) -> pd.DataFrame:
+    df_num_unique_teams_by_league = data.groupby(by=['League']).apply(get_num_unique_teams).reset_index()
+    df_num_unique_teams_by_league.rename(mapper={0: 'NumUniqueTeams'}, axis=1, inplace=True)
+    return df_num_unique_teams_by_league
