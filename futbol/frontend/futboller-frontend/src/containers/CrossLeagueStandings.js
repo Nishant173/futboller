a87118d59d87cd1b64a8d0253a86f5cc6d7800b9
Nishant173 <nishant.rao173@gmail.com>
2021-01-27 21:35:13 +0530

    Added class based container - CrossLeagueStandings

---
+++ futbol/frontend/futboller-frontend/src/containers/CrossLeagueStandings.js
@@ -0,0 +1,127 @@
+import React from 'react'
+
+import { getCrossLeagueStandings } from '../api/getApiData'
+
+import { GridTable } from '../components/tables/Table'
+import { ColumnsCrossLeagueTable, ColumnsCrossLeagueStats } from '../components/tables/TableColumns'
+
+import { ScatterChart } from '../components/charts/ScatterChart'
+
+import {
+    ceil,
+    generateRandomHexCode,
+    getValuesByKey,
+    max,
+    maxOfAbsValues,
+} from '../jsUtils/general'
+
+
+export default class CrossLeagueStandings extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: [],
+            teams: [],
+            avgPoints: [],
+            avgGoalDifference: [],
+            avgGoalsScored: [],
+            avgGoalsAllowed: [],
+        }
+        this.updateData = this.updateData.bind(this)
+        this.updateWrangledData = this.updateWrangledData.bind(this)
+        this.sliceByPosition = this.sliceByPosition.bind(this)
+        this.sliceTop80 = this.sliceTop80.bind(this)
+    }
+
+    updateData() {
+        getCrossLeagueStandings()
+            .then((response) => {
+                this.setState({
+                    data: response,
+                })
+                this.updateWrangledData()
+            })
+    }
+
+    updateWrangledData() {
+        this.setState({
+            teams: getValuesByKey(this.state.data, "team"),
+            avgPoints: getValuesByKey(this.state.data, "avgPoints"),
+            avgGoalDifference: getValuesByKey(this.state.data, "avgGoalDifference"),
+            avgGoalsScored: getValuesByKey(this.state.data, "avgGoalsScored"),
+            avgGoalsAllowed: getValuesByKey(this.state.data, "avgGoalsAllowed"),
+        })
+    }
+
+    sliceByPosition(array, start, stop) {
+        return array.slice(start-1, stop)
+    }
+
+    sliceTop80(array) {
+        return this.sliceByPosition(array, 1, 80)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Cross League Standings - Top 5 Leagues</h1>
+                <br />
+
+                <form className="cross-league-table-form">
+                    <input
+                        type="button"
+                        value="Re-load"
+                        onClick={this.updateData}
+                    />
+                </form>
+
+                {
+                    this.state.data.length > 0 ? 
+                    <>
+                        <br /><br />
+                        <GridTable
+                            arrayOfObjects={this.state.data}
+                            columnsData={ColumnsCrossLeagueTable}
+                        />
+                        <br /><br />
+                        <GridTable
+                            arrayOfObjects={this.state.data}
+                            columnsData={ColumnsCrossLeagueStats}
+                        />
+                        <br /><br />
+                        <ScatterChart
+                            title="AvgPoints vs AvgGoalDifference"
+                            xLabel="AvgPoints"
+                            yLabel="AvgGoalDifference"
+                            arrayOfObjects={this.sliceTop80(this.state.data)}
+                            scatterLabelsArray={this.sliceTop80(this.state.teams)}
+                            xObj="avgPoints"
+                            yObj="avgGoalDifference"
+                            xLow={0}
+                            xHigh={ceil(max(this.sliceTop80(this.state.avgPoints)))}
+                            yLow={-maxOfAbsValues(this.sliceTop80(this.state.avgGoalDifference))}
+                            yHigh={maxOfAbsValues(this.sliceTop80(this.state.avgGoalDifference))}
+                            color={generateRandomHexCode()}
+                        />
+                        <br /><br />
+                        <ScatterChart
+                            title="AvgGoalsScored vs AvgGoalsAllowed"
+                            xLabel="AvgGoalsScored"
+                            yLabel="AvgGoalsAllowed"
+                            arrayOfObjects={this.sliceTop80(this.state.data)}
+                            scatterLabelsArray={this.sliceTop80(this.state.teams)}
+                            xObj="avgGoalsScored"
+                            yObj="avgGoalsAllowed"
+                            xLow={0}
+                            xHigh={ceil(max(this.sliceTop80(this.state.avgGoalsScored)))}
+                            yLow={0}
+                            yHigh={ceil(max(this.sliceTop80(this.state.avgGoalsAllowed)))}
+                            color={generateRandomHexCode()}
+                        />
+                    </>
+                    : null
+                }
+            </div>
+        )
+    }
+}
