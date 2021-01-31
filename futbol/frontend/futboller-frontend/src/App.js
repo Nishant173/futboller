496e251e82257159377cea371d1720d529274818
Nishant173 <nishant.rao173@gmail.com>
2021-01-31 19:36:26 +0530

    Updated App.js

--- futbol/frontend/futboller-frontend/src/App.js
+++ futbol/frontend/futboller-frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Navbar from './components/navbar/Navbar'
 import Home from './containers/HomePage'
 import LeagueStandings from './containers/LeagueStandings'
+import LeagueHeadToHeadStats from './containers/LeagueHeadToHeadStats'
 import CrossLeagueStandings from './containers/CrossLeagueStandings'
 import CrossLeagueStatsByTeam from './containers/CrossLeagueStatsByTeam'
 
@@ -16,6 +17,7 @@ export default function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/league-standings" component={LeagueStandings} />
+          <Route exact path="/league-h2h-stats" component={LeagueHeadToHeadStats} />
           <Route exact path="/cross-league-standings" component={CrossLeagueStandings} />
           <Route exact path="/cross-league-stats-by-team" component={CrossLeagueStatsByTeam} />
         </Switch>
