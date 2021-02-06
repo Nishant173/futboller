91ad14f18adbe3a87c6d28dec904caaf25c4e43c
Nishant173 <nishant.rao173@gmail.com>
2021-02-06 12:50:49 +0530

    Refactored queries in 'leagues' app

--- futbol/leagues/queries.py
+++ futbol/leagues/queries.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Union
 import pandas as pd
 import sqlite3
 
@@ -6,15 +6,21 @@ from futbol import config
 from .utils import get_unique_teams
 
 
+def _get_data_from_sqlite(query: str) -> Union[pd.DataFrame, pd.Series]:
+    """Gets Pandas object (DataFrame or Series) from given query (via the SQLite database)"""
+    connection = sqlite3.connect(database=config.DB_FILEPATH)
+    df = pd.read_sql(sql=query, con=connection)
+    connection.close()
+    return df
+
+
 def get_teams() -> List[str]:
     """Returns list of teams from `leagues_leaguematch` table"""
-    sql = f"""
+    query = f"""
     SELECT home_team, away_team
     FROM {config.TBL_LEAGUE_MATCHES}
     """
-    connection = sqlite3.connect(database=config.DB_FILEPATH)
-    df = pd.read_sql(sql=sql, con=connection)
-    connection.close()
+    df = _get_data_from_sqlite(query=query)
     if df.empty:
         return []
     teams = get_unique_teams(data=df)
@@ -23,14 +29,12 @@ def get_teams() -> List[str]:
 
 def get_leagues() -> List[str]:
     """Returns list of league names from `leagues_leaguematch` table"""
-    sql = f"""
+    query = f"""
     SELECT DISTINCT(league)
     FROM {config.TBL_LEAGUE_MATCHES}
     ORDER BY league ASC
     """
-    connection = sqlite3.connect(database=config.DB_FILEPATH)
-    df = pd.read_sql(sql=sql, con=connection)
-    connection.close()
+    df = _get_data_from_sqlite(query=query)
     if df.empty:
         return []
     leagues = df['league'].tolist()
@@ -39,14 +43,12 @@ def get_leagues() -> List[str]:
 
 def get_seasons() -> List[str]:
     """Returns list of season names from `leagues_leaguematch` table"""
-    sql = f"""
+    query = f"""
     SELECT DISTINCT(season)
     FROM {config.TBL_LEAGUE_MATCHES}
     ORDER BY season ASC
     """
-    connection = sqlite3.connect(database=config.DB_FILEPATH)
-    df = pd.read_sql(sql=sql, con=connection)
-    connection.close()
+    df = _get_data_from_sqlite(query=query)
     if df.empty:
         return []
     seasons = df['season'].tolist()
