bdd3b01fae3c746524a59f6394c26e3c2335ac71
Nishant173 <nishant.rao173@gmail.com>
2021-03-13 19:37:42 +0530

    Updated combined reducers

--- futbol/frontend/futboller-frontend/src/combinedReducers.js
+++ futbol/frontend/futboller-frontend/src/combinedReducers.js
@@ -1,7 +1,15 @@
-import LeagueStandingsDataReducer from './store/reducers/LeagueStandingsReducer'
+import LeagueStandingsReducer from './store/reducers/LeagueStandingsReducer'
+import CrossLeagueStandingsReducer from './store/reducers/CrossLeagueStandingsReducer'
+import GoalRelatedStatsReducer from './store/reducers/GoalRelatedStatsReducer'
+import HeadToHeadStatsReducer from './store/reducers/HeadToHeadStatsReducer'
+import PartitionedStatsReducer from './store/reducers/PartitionedStatsReducer'
 
 const combinedReducers = {
-    LeagueStandingsDataReducer: LeagueStandingsDataReducer,
+    LeagueStandingsReducer: LeagueStandingsReducer,
+    CrossLeagueStandingsReducer: CrossLeagueStandingsReducer,
+    GoalRelatedStatsReducer: GoalRelatedStatsReducer,
+    HeadToHeadStatsReducer: HeadToHeadStatsReducer,
+    PartitionedStatsReducer: PartitionedStatsReducer,
 }
 
 export default combinedReducers
