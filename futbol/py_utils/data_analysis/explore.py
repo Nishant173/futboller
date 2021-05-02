30071886f58d1b17f1fa6e6149e792c019d854b8
Nishant173 <nishant.rao173@gmail.com>
2021-05-02 10:51:24 +0530

    Updated PyUtils module

--- futbol/py_utils/data_analysis/explore.py
+++ futbol/py_utils/data_analysis/explore.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Dict, List, Optional, Union
 
 import pandas as pd
 
@@ -8,6 +8,40 @@ def is_full(data: pd.DataFrame) -> bool:
     is_not_empty = (not data.empty)
     has_no_missing_values = (data.isnull().sum().sum() == 0)
     return (is_not_empty & has_no_missing_values)
+
+
+def get_column_availability_info(data: pd.DataFrame,
+                                 expected_columns: List[Union[int, float, str]]) -> Dict[str, List[str]]:
+    """
+    Takes in non-empty DataFrame, and list of columns expected to be in said DataFrame.
+    Returns dictionary having 2 keys: ['columns_available', 'columns_missing'], wherein the value for
+    each key will be a list of columns that are available/missing.
+    """
+    if data.empty:
+        raise ValueError("Expects non-empty DataFrame")
+    all_columns = data.columns.tolist()
+    expected_columns = list(expected_columns)
+    columns_available = list(
+        set(all_columns).intersection(set(expected_columns))
+    )
+    columns_missing = list(
+        set(all_columns).difference(set(columns_available))
+    )
+    dict_obj = {
+        'columns_available': columns_available,
+        'columns_missing': columns_missing,
+    }
+    return dict_obj
+
+
+def has_all_expected_columns(data: pd.DataFrame,
+                             expected_columns: List[Union[int, float, str]]) -> bool:
+    """Returns True if all expected columns are available in given DataFrame; otherwise returns False"""
+    dict_column_availability_info = get_column_availability_info(
+        data=data,
+        expected_columns=expected_columns,
+    )
+    return len(dict_column_availability_info['columns_missing']) == 0
 
 
 def describe_missing_data(data: pd.DataFrame,
